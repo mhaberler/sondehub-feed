9e9b1451d935e7695b4ca603a631df74b668afc6
Michael Haberler <git@mah.priv.at>
2021-03-02 08:09:31 +0100

    switch to use pyondehub

--- sondehub-republish.py
+++ sondehub-republish.py
@@ -1,14 +1,14 @@
 # pip3 install boto3 paho-mqtt
 
-import hashlib
-import datetime
-import hmac
-import boto3
-import time
-import uuid
-import paho.mqtt.client as mqtt
-from urllib.parse import urlparse
-import urllib.parse
+# import hashlib
+# import hmac
+# import boto3
+# import time
+# import uuid
+# import paho.mqtt.client as mqtt
+# from urllib.parse import urlparse
+# import urllib.parse
+import sondehub
 import zmq
 import logging
 import logging.handlers as handlers
@@ -20,6 +20,8 @@ import bz2
 import signal
 import setproctitle
 import geojson
+import json
+import datetime
 
 appName = "sondehub-republisher"
 defaultLoglevel = 'INFO'
@@ -28,67 +30,10 @@ minDelay = 3
 maxDelay = 120
 reconnectPause = 10
 
-def aws_sign(key, msg):
-    return hmac.new(key, msg.encode('utf-8'), hashlib.sha256).digest()
 
-
-def aws_getSignatureKey(key, dateStamp, regionName, serviceName):
-    kDate = aws_sign(('AWS4' + key).encode('utf-8'), dateStamp)
-    kRegion = aws_sign(kDate, regionName)
-    kService = aws_sign(kRegion, serviceName)
-    kSigning = aws_sign(kService, 'aws4_request')
-    return kSigning
-
-
-def aws_presign(access_key=None, secret_key=None, session_token=None, host=None, region=None, method=None, protocol=None, uri=None, service=None, expires=3600, payload_hash=None):
-    # method=GET, protocol=wss, uri=/mqtt service=iotdevicegateway
-    assert 604800 >= expires >= 1, "Invalid expire time 604800 >= %s >= 1" % expires
-
-    # Date stuff, first is datetime, second is just date.
-    t = datetime.datetime.utcnow()
-    date_time = t.strftime('%Y%m%dT%H%M%SZ')
-    date = t.strftime('%Y%m%d')
-    # Signing algorithm used
-    algorithm = 'AWS4-HMAC-SHA256'
-
-    # Scope of credentials, date + region (eu-west-1) + service (iot gateway hostname) + signature version
-    credential_scope = date + '/' + region + '/' + service + '/' + 'aws4_request'
-    # Start building the query-string
-    canonical_querystring = 'X-Amz-Algorithm=' + algorithm
-    canonical_querystring += '&X-Amz-Credential=' + \
-        urllib.parse.quote_plus(access_key + '/' + credential_scope)
-    canonical_querystring += '&X-Amz-Date=' + date_time
-    canonical_querystring += '&X-Amz-Expires=' + str(expires)
-    canonical_querystring += '&X-Amz-SignedHeaders=host'
-
-    if payload_hash is None:
-        if service == 'iotdevicegateway':
-            payload_hash = hashlib.sha256(b'').hexdigest()
-        else:
-            payload_hash = 'UNSIGNED-PAYLOAD'
-
-    canonical_headers = 'host:' + host + '\n'
-    canonical_request = method + '\n' + uri + '\n' + canonical_querystring + \
-        '\n' + canonical_headers + '\nhost\n' + payload_hash
-
-    string_to_sign = algorithm + '\n' + date_time + '\n' + credential_scope + \
-        '\n' + hashlib.sha256(canonical_request.encode()).hexdigest()
-    signing_key = aws_getSignatureKey(secret_key, date, region, service)
-    signature = hmac.new(signing_key, string_to_sign.encode(
-        'utf-8'), hashlib.sha256).hexdigest()
-
-    canonical_querystring += '&X-Amz-Signature=' + signature
-    if session_token:
-        canonical_querystring += '&X-Amz-Security-Token=' + \
-            urllib.parse.quote(session_token)
-
-    return protocol + '://' + host + uri + '?' + canonical_querystring
-
-
-class SondeRepublisher(mqtt.Client):
+class SondeRepublisher(sondehub.Stream):
 
     def __init__(self,
-                 clientId=str(uuid.uuid4()),
                  pubSocketURI=None,
                  dealerSocketURI=None,
                  logger=None,
@@ -107,39 +52,24 @@ class SondeRepublisher(mqtt.Client):
         else:
             self.dealerSocket = None
 
-        mqtt.Client.__init__(self, client_id=clientId,
-                             clean_session=clean_session, transport="websockets")
-        self.enable_logger(logger=logger)
-        log.debug(f"client_id={clientId} clean_session={clean_session}")
-
-    def do_connect(self,
-                   topic="sondes",
-                   urlparts=None,
-                   headers=None,
-                   port=443,
-                   keepalive=60):
-        self.topic = topic
-        self.ws_set_options(
-            path=f"{urlparts.path}?{urlparts.query}", headers=headers)
-        self.tls_set()
-        self.reconnect_delay_set(min_delay=minDelay, max_delay=maxDelay)
-        self.connect(urlparts.netloc, port, keepalive)
+        self.shstream = sondehub.Stream.__init__(self,
+                                                 on_connect=self.on_connect,
+                                                 on_message=self.on_message)
 
     def run(self, *args, **kwargs):
         log.debug(f"loop_forever")
-        self.loop_forever(**kwargs)
+        self.mqttc.loop_forever(**kwargs)
 
-    def on_connect(self, mqttc, obj, flags, rc):
-        log.info(f"on_connect rc={rc}")
-        self.subscribe(self.topic, 1)
+    def on_connect(self):
+        log.info(f"on_connect")
 
-    def on_message(self, mqttc, obj, msg):
-        log.debug(
-            f"on_message topic={msg.topic} qos={msg.qos} payload={str(msg.payload)}")
-        self.pubSocket.send_multipart([msg.topic.encode(), msg.payload])
+    def on_message(self, msg):
+        log.debug(f"on_message msg={msg}")
+        m = json.dumps(msg, indent=4).encode("utf8")
+        self.pubSocket.send_multipart([b'decoded',m])
         if self.dealerSocket:
             try:
-                self.dealerSocket.send(msg.payload, zmq.DONTWAIT)
+                self.dealerSocket.send(m, zmq.DONTWAIT)
             except zmq.error.Again:
                 pass
 
@@ -152,11 +82,11 @@ class SondeRepublisher(mqtt.Client):
     def on_log(self, mqttc, obj, level, string):
         log.log(level, f"on_log msg='{string}'")
 
-    def on_disconnect(self, client, userdata, rc):
-        log.warn(f"Unexpected MQTT disconnection: userdata={userdata} reason={rc}, exit for restart")
-        sys.exit(0) # let systemd do the restart
-        # time.sleep(reconnectPause)
-        # self.reconnect()
+    # def on_disconnect(self, client, userdata, rc):
+    #     log.warn(f"Unexpected MQTT disconnection: userdata={userdata} reason={rc}, exit for restart")
+    #     sys.exit(0) # let systemd do the restart
+    #     # time.sleep(reconnectPause)
+    #     # self.reconnect()
 
 def str2bool(v):
     if isinstance(v, bool):
@@ -220,34 +150,6 @@ def main():
         description='receive sondehub feed and republish via zmq',
         add_help=True)
 
-    parser.add_argument('--sondehub-host',
-                        dest='sondehubHost',
-                        action='store',
-                        default=os.environ.get('SONDEHUB_HOST'),
-                        type=str,
-                        help='host to connect to')
-
-    parser.add_argument('--sondehub-pool-id',
-                        dest='sondehubPoolId',
-                        action='store',
-                        default=os.environ.get('SONDEHUB_POOLID'),
-                        type=str,
-                        help='sondehub identity pool id')
-
-    parser.add_argument('--sondehub-region',
-                        dest='sondehubRegion',
-                        action='store',
-                        default=os.environ.get('SONDEHUB_REGION'),
-                        type=str,
-                        help='AWS region')
-
-    parser.add_argument('--client-id',
-                        dest='clientId',
-                        action='store',
-                        default=os.environ.get('SONDEHUB_CLIENTID'),
-                        type=str,
-                        help='republishing socket like ipc:///tmp/sondehub-feed or tcp://127.0.0.1:5001')
-
     parser.add_argument('--pub-socket',
                         dest='pubSocket',
                         action='store',
@@ -277,10 +179,6 @@ def main():
                                  'WARNING', 'ERROR', 'CRITICAL'],
                         dest="logLevel")
 
-    parser.add_argument("--persistent", type=str2bool, nargs='?',
-                        default=os.environ.get('SONDEHUB_PERSISTENT', True),
-                        const=False, dest='clean_session',
-                        help="tell broker to keep queued messages (clean_session=False)")
 
     args = parser.parse_args()
     setup_logging(getattr(logging, args.logLevel), appName, args.logDir)
@@ -288,43 +186,15 @@ def main():
 
     log.info(f"starting up")
 
-    identityPoolID = f"{args.sondehubRegion}:{args.sondehubPoolId}"
-    cognitoIdentityClient = boto3.client(
-        'cognito-identity', region_name=args.sondehubRegion)
-    temporaryIdentityId = cognitoIdentityClient.get_id(
-        IdentityPoolId=identityPoolID)
-    identityID = temporaryIdentityId["IdentityId"]
-
-    temporaryCredentials = cognitoIdentityClient.get_credentials_for_identity(
-        IdentityId=identityID)
-    AccessKeyId = temporaryCredentials["Credentials"]["AccessKeyId"]
-    SecretKey = temporaryCredentials["Credentials"]["SecretKey"]
-    SessionToken = temporaryCredentials["Credentials"]["SessionToken"]
-
-    url = aws_presign(access_key=AccessKeyId,
-                      secret_key=SecretKey,
-                      session_token=SessionToken,
-                      method="GET",
-                      protocol="wss",
-                      uri="/mqtt",
-                      service="iotdevicegateway",
-                      host=args.sondehubHost,
-                      region=args.sondehubRegion)
-    urlparts = urlparse(url)
-    headers = {
-        "Host": "{0:s}".format(urlparts.netloc),
-    }
     sp = SondeRepublisher(pubSocketURI=args.pubSocket,
                           dealerSocketURI=args.dealerSocket,
-                          logger=log,
-                          clean_session=args.clean_session,
-                          clientId=args.clientId)
-    sp.do_connect(urlparts=urlparts, headers=headers)
-    setproctitle.setproctitle((f"{appName} log={log.getEffectiveLevel()} "
-                               f"logdir={args.logDir} "
-                               f"host={args.sondehubHost}"))
-    sp.run(retry_first_connection=True, timeout=20.0)
+                          logger=log)
 
+    setproctitle.setproctitle((f"{appName} log={log.getEffectiveLevel()} "
+                               f"logdir={args.logDir} "))
+    #sp.run(retry_first_connection=True, timeout=20.0)
+    while 1:
+        pass
 
 if __name__ == '__main__':
     main()
